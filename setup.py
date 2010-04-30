f48feec2c2da5b1b4e234b97cf7d8bba9737233a
Ben Bangert <ben@groovie.org>
2010-04-30 12:24:56 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='velruse',
+      version=version,
+      description="Simplifying third-party authentication for web applications.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Ben Bangert',
+      author_email='ben@groovie.org',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
