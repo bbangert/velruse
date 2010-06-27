9298a4f5732e3df48630c494cf1e76934200a0c0
Ian Wilson <ianjosephwilson@gmail.com>
2010-06-27 15:00:21 -0700

    Add pyyaml dependency.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='velruse',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          "WebOb>=0.9.8", "python-openid>=2.2.4", "nose>=0.11", "oauth2>=1.1.3",
+          "WebOb>=0.9.8", "python-openid>=2.2.4", "nose>=0.11", "oauth2>=1.1.3", "pyyaml",
       ],
       entry_points="""
       [paste.app_factory]
