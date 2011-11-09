2c6e2759b7175733dca834e0700afc1c07811e88
Michael Merickel <michael@merickel.org>
2011-11-09 10:06:15 -0600

    Updated to show credentials in demo.

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -2,7 +2,10 @@ from pyramid.view import view_config
 
 @view_config(context='velruse.api.AuthenticationComplete', renderer='json')
 def auth_complete_view(context, request):
-    return context.profile
+    return {
+        'profile': context.profile,
+        'credentials': context.credentials,
+    }
 
 
 @view_config(context='velruse.exceptions.AuthenticationDenied',
