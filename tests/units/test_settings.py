287afce3eb512d79ed96e0a663b0074850ea78c9
Michael Merickel <michael@merickel.org>
2012-08-09 00:19:35 -0500

    added test for bool conversion

--- tests/units/test_settings.py
+++ tests/units/test_settings.py
@@ -2,7 +2,7 @@ from velruse.testing import unittest
 
 class TestProviderSettings(unittest.TestCase):
 
-    def _makeOne(self, settings, prefix):
+    def _makeOne(self, settings, prefix=''):
         from velruse.settings import ProviderSettings
         return ProviderSettings(settings, prefix=prefix)
 
@@ -15,3 +15,9 @@ class TestProviderSettings(unittest.TestCase):
         self.assertRaises(KeyError, p.update, 'missing', required=True)
         p.update('missing')
         self.assertEqual(p.kwargs, {'foo': 'bar', 'baz': 'bar'})
+
+    def test_bool(self):
+        p = self._makeOne({'is_true': 'true', 'is_false': 'false'})
+        p.update('is_true')
+        p.update('is_false')
+        self.assertEqual(p.kwargs, {'is_true': True, 'is_false': False})
