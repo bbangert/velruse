32ad32ebaf645f7252f3cfeb754b4ac518abd668
Michael Merickel <michael@merickel.org>
2012-10-11 22:13:51 -0500

    prep 1.0.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ docs_extras = [
 ]
 
 setup(name='velruse',
-      version='1.0.2',
+      version='1.0.3',
       description=(
           'Simplifying third-party authentication for web applications.'),
       long_description='',
