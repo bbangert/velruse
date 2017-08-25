c48d05dbc45201cfb89065ae2bc6554625e36faf
BaardBouvet <baard.johansen@bouvet.no>
2017-08-25 10:16:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ except IOError:
     README = CHANGES = ''
 
 setup(name='velruse',
-      version='1.1.1',
+      version='1.1.1a',
       description=(
           'Simplifying third-party authentication for web applications.'),
       long_description=README + '\n\n' + CHANGES,
